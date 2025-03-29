f78c3142e272d559b3ab383f8b8c378331babefa
Varsha99-sys <sangalepearl99@gmail.com>
2025-03-29 08:57:45 +0000

    design

--- elibrary_project/elibrary_project/settings.py
+++ elibrary_project/elibrary_project/settings.py
@@ -119,6 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+import os
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'elibrary_app/static')
+]
+
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[BASE_DIR/"static"]
 
